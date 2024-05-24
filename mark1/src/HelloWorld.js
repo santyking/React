803a2121dcac33f7dd11ca146d236438dec93ec3
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-05-25 00:56:22 +0530

    hello world

---
+++ mark1/src/HelloWorld.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function HelloWorld() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello World
+      </header>
+    </div>
+  );
+}
+
+export default HelloWorld;
