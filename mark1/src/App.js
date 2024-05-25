b8efd10aa22ccde361cab85f9e366112a38ea85b
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-05-25 23:59:03 +0530

    another method

--- mark1/src/App.js
+++ mark1/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+// import React from 'react'
 // import {User} from './User';
 import ClassComponent from './ClassComponent';
 
@@ -14,5 +15,13 @@ function App() {
     </div>
   );
 }
+
+// class ClassComponent extends React.Component{
+//   render(){
+//       return(
+//           <h1>Hello from Class User Component</h1>
+//       )
+//   }
+// }
 
 export default App;
