5bf3c3ba891cc718e74a32d282e31e5deacbe9ad
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-05-25 23:55:30 +0530

    Class Compoenent

--- mark1/src/App.js
+++ mark1/src/App.js
@@ -1,25 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
 // import {User} from './User';
+import ClassComponent from './ClassComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello main Component
-        <User></User>
+        {/* Hello main Component
+        <User></User> */}
+        <ClassComponent></ClassComponent>
       </header>
     </div>
   );
 }
 
 export default App;
-
-function User() {
-  return(
-      <div>
-          <h1>This is User Component</h1>
-          <h1>This is User Component</h1>
-      </div>
-  )
-}
