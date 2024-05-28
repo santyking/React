d0cfa19e2ecd8928374141e15006b2083cecb986
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-05-28 23:27:24 +0530

    On Button Click Data is being updated

--- inputboxvalue/src/App.js
+++ inputboxvalue/src/App.js
@@ -4,15 +4,23 @@ import React,{useState} from 'react'
 
 function App() {
   const [data,setData] = useState(null);
+  const [print,setPrint] = useState(false);
   function updateData(val) {
     console.log(val.target.value);
     setData(val.target.value);
+    setPrint(false);
   }
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{data}</h1>
+        {
+          print?
+          <h1> {data}</h1>
+          :null
+        }
         <input type="text" onChange={updateData}/>
+        {/* <input type="text" /> */}
+        <button onClick={()=>{setPrint(true)}}>Update Data</button>
       </header>
     </div>
   );
