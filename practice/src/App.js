e0eaab207dad8d7ba4fe2e57d5d71ec57751fe65
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-05-30 23:49:56 +0530

    initial commit

--- practice/src/App.js
+++ practice/src/App.js
@@ -2,21 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function updateData(val) {
+    console.log(val.target.value);
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Header</h1>
+        <input type="text" onChange={updateData}/>
       </header>
     </div>
   );
