fd77ddefa394f102ac65247c632d677487b2c689
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-05-31 23:54:53 +0530

    Update App.js

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import {usestate} from react;
 
 function App() {
+const [data,setdata] = usestate(null);
   function updateData(val) {
     console.log(val.target.value);
-    
+    setdata(val.target.value);
   }
   return (
     <div className="App">
