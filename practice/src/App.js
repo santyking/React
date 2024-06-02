ac2c009ae29b00cbb8141b1adb3e24976dace006
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-06-02 23:45:29 +0530

    fixed code

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import {usestate} from react;
+import React,{useState} from 'react'
 
 function App() {
-const [data,setdata] = usestate(null);
+const [data,setdata] = useState(null);
   function updateData(val) {
     console.log(val.target.value);
     setdata(val.target.value);
@@ -11,7 +11,7 @@ const [data,setdata] = usestate(null);
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Header</h1>
+        <h1>{data}</h1>
         <input type="text" onChange={updateData}/>
       </header>
     </div>
