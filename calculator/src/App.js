3461fdb3d15363c202e1a12e9dd9c372d7b97978
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2024-06-06 00:30:03 +0530

    added validation

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,38 +1,42 @@
-// import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import React,{useState} from 'react'
+
 function App() {
   const date = new Date();
-  let day = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
-  const [datum,setDatum]  = useState(null);
-  const [print,setPrint] = useState(false);
+  const [datum, setDatum] = useState(null);
+  const [print, setPrint] = useState(false);
+
   function calculateDate(val) {
     const givenDate = new Date(val.target.value);
-  if (givenDate.valueOf() < date.valueOf()) {
-    const diffTime = Math.abs(givenDate - date);
-    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-    setDatum(diffDays + 1);
-    setPrint(false);
+    if (givenDate.valueOf() < date.valueOf()) {
+      const diffTime = Math.abs(givenDate - date);
+      const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+      setDatum(diffDays + 1);
+      setPrint(true);
+    } else {
+      setDatum(null);
+      setPrint(false);
+    }
   }
 
-
-  }
   return (
     <div className="App">
       <header className="App-header">
-      <h3>Todays Date is {day}-{month}-{year}</h3> 
-      <label htmlFor="">Enter Birth Date:</label>
-      <input type="date" onChange={calculateDate}/>
-      <button onClick={()=>{setPrint(true)}}>Calulate Age</button>
-      {
-        print
-          ?
-        <h4>Op is {datum} Days</h4>
-          :
-        null
-      } 
+        <h3>
+          Today's Date is {date.getDate()}-{date.getMonth() + 1}-{date.getFullYear()}
+        </h3>
+        <label htmlFor="">Enter Birth Date:</label>
+        <input type="date" onChange={calculateDate} />
+        <button onClick={() => setPrint(true)}>Calculate Age</button>
+        {print && (
+          <>
+            {datum ? (
+              <h4>Age is {datum} Days</h4>
+            ) : (
+              <h4>Given date is greater than today</h4>
+            )}
+          </>
+        )}
       </header>
     </div>
   );
