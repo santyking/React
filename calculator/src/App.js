6dc0262954c148044951b68f63c9c0bdd84c3f23
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-06-07 00:41:24 +0530

    Added years months days

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -9,14 +9,31 @@ function App() {
   function calculateDate(val) {
     const givenDate = new Date(val.target.value);
     if (givenDate.valueOf() < date.valueOf()) {
-      const diffTime = Math.abs(givenDate - date);
-      const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-      setDatum(diffDays + 1);
+      const diff = calculateAge(givenDate, date);
+      setDatum(diff);
       setPrint(true);
     } else {
       setDatum(null);
       setPrint(false);
     }
+  }
+
+  function calculateAge(birthDate, currentDate) {
+    let years = currentDate.getFullYear() - birthDate.getFullYear();
+    let months = currentDate.getMonth() - birthDate.getMonth();
+    let days = currentDate.getDate() - birthDate.getDate();
+
+    if (days < 0) {
+      months -= 1;
+      days += new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+    }
+
+    if (months < 0) {
+      years -= 1;
+      months += 12;
+    }
+
+    return { years, months, days };
   }
 
   return (
@@ -27,11 +44,10 @@ function App() {
         </h3>
         <label htmlFor="">Enter Birth Date:</label>
         <input type="date" onChange={calculateDate} />
-        <button onClick={() => setPrint(true)}>Calculate Age</button>
         {print && (
           <>
             {datum ? (
-              <h4>Age is {datum} Days</h4>
+              <h4>Age is {datum.years} Years {datum.months} Months {datum.days} Days</h4>
             ) : (
               <h4>Given date is greater than today</h4>
             )}
