9eab0d96aad6fc96027b2c7886d0a61810af4722
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-06-09 00:32:09 +0530

    fixed age issue

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -3,18 +3,28 @@ import './App.css';
 
 function App() {
   const date = new Date();
+  const today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
   const [datum, setDatum] = useState(null);
   const [print, setPrint] = useState(false);
+  const [displayDate, setDisplayDate] = useState(null);
 
   function calculateDate(val) {
     const givenDate = new Date(val.target.value);
-    if (givenDate.valueOf() < date.valueOf()) {
-      const diff = calculateAge(givenDate, date);
+    const givenDateNormalized = new Date(givenDate.getFullYear(), givenDate.getMonth(), givenDate.getDate());
+
+    if (givenDateNormalized < today) {
+      const diff = calculateAge(givenDateNormalized, today);
       setDatum(diff);
       setPrint(true);
-    } else {
-      setDatum(0);
+      setDisplayDate(2);
+    } else if (givenDateNormalized.getTime() === today.getTime()) {
+      setDatum(null);
       setPrint(true);
+      setDisplayDate(1);
+    } else {
+      setDatum(null);
+      setPrint(true);
+      setDisplayDate(0);
     }
   }
 
@@ -36,8 +46,6 @@ function App() {
     return { years, months, days };
   }
 
-  console.log(datum);
-
   return (
     <div className="App">
       <header className="App-header">
@@ -46,17 +54,17 @@ function App() {
         </h1>
         <>
           <label htmlFor="birthdate"><h3>Enter Birth date</h3></label>
-          <input id ="birthdate" type="date" onChange={calculateDate} />
+          <input id="birthdate" type="date" onChange={calculateDate} />
         </>
-        {print && (
-          <>
-            {(datum !== 0) ? 
-              <h4>Age is {datum.years} Years {datum.months} Months {datum.days} Days</h4>
-             : (
-              <h4>Given date is greater than today</h4>
-            )}
-          </>
-        )}
+        {
+          print && (
+            displayDate === 2 ? 
+            <h4>Age is {datum.years} Years {datum.months} Months {datum.days} Days</h4>
+            : displayDate === 1 ? 
+            <h4>You are born today!</h4>
+            : <h4>Born in the future</h4>
+          )
+        }
       </header>
     </div>
   );
