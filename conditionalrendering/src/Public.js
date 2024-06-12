145eb84b21ae402bc2832305bbaf4213b248b82b
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-06-12 23:58:48 +0530

    Intial commit

---
+++ conditionalrendering/src/Public.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Public() {
+    return (
+        <div className="App">
+            <header className="App-header">
+
+            </header>
+        </div>
+    );
+}
+
+export default Public;
