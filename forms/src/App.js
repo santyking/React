0f728db3321ef99313266f63dbb24af416fb5dfe
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-06-12 23:30:52 +0530

    Forms Completed

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from 'react'
 
 function App() {
-  function submitDetails(e) {
-    
+  const [name, SetName] = useState(null);
+  const [comics, SetComics] = useState(null);
+  const [agree, SetAgree] = useState(null);
+  function getData(e) {
+    console.log(name, comics, agree);
+    e.preventDefault();
   }
   return (
     <div className="App">
       <header className="App-header">
         <h1>Demo Form</h1>
-        <label htmlFor="nameid">Name</label>
-        <input type="text" id='nameid'/>
-        <select name="comics" id="comics">
-          <option value="Select Comics">Select Comics</option>
-          <option value="Marvel">Marvel</option>
-          <option value="DC">DC</option>
-        </select>
-        <input type="checkbox" name="agree" id="agree" />
-        <span>Agree</span>
-        <button type="submit" onClick={submitDetails}>Submit</button>
+        <form onSubmit={getData}>
+          <label htmlFor="nameid">Name</label>
+          <input type="text" id="nameid" onChange={(e) => SetName(e.target.value)} /><br></br>
+          <select name="comics" id="comics" onChange={(e) => SetComics(e.target.value)}>
+            <option value="Select Comics">Select Comics</option>
+            <option value="Marvel">Marvel</option>
+            <option value="DC">DC</option>
+          </select><br></br>
+          <input type="checkbox" name="agree" id="agree" onChange={(e) => SetAgree(e.target.checked)} />
+          <span>Agree</span><br></br>
+          <button type="submit">
+            Submit
+          </button>
+        </form>
       </header>
     </div>
   );
