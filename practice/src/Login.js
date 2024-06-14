7d98662b940dfaeda81cca25800527bc3acdbbe9
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-06-14 23:58:18 +0530

    practice on 14 June 2024

---
+++ practice/src/Login.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+export default function Login() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Login Page</h1>
+                <form action="">
+                    
+                </form>
+            </header>
+        </div>
+    )
+}
