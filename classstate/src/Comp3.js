ddfebb229e55dd397c646c397d38dc2ce6f7ac3d
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-06-22 00:05:22 +0530

    practice toggle for class component

--- classstate/src/Comp3.js
+++ classstate/src/Comp3.js
@@ -3,14 +3,18 @@ export default class Comp3 extends Component{
     constructor(){
         super();
         this.state = {
-            data: "Santosh"
-        }   
+            data: "Santosh",
+            hide: true
+        };   
     }
     render(){
         return(
             <>
+                {this.state.hide?
                 <h1>{this.state.data}</h1>
+                :null}
                 <button onClick={()=>this.setState({data: "Anvi"})}>Change Name</button>
+                <button onClick={()=>this.setState({hide:!this.state.hide})}>Toggle</button>
             </>
         )
     }
