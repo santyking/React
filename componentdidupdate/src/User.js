be92d723cd96ed000fe5265e15bd34cf604bde62
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-06-24 23:56:05 +0530

    Component did update

---
+++ componentdidupdate/src/User.js
@@ -0,0 +1,25 @@
+import React from "react";
+export default class User extends React.Component{
+    constructor(){
+        console.warn("Constructor");
+        super();
+        this.state = {
+            data: "Santosh"
+        }
+    }
+    componentDidMount(){
+        console.warn("Component did Mount");
+    }
+    componentDidUpdate(){
+        console.warn("Component Did Update");
+    }
+    render(){
+        console.warn("Render");
+        return(
+            <>
+                <h1>{this.state.data}</h1>
+                <button onClick={()=>this.setState({data:"Anvi"})}>Change Name</button>
+            </>
+        );
+    }
+}
