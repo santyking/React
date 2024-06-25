aa5395a711c280af02d3dd66419b6417a602f23a
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-06-26 00:11:40 +0530

    using counter for component did update

--- componentdidupdate/src/User.js
+++ componentdidupdate/src/User.js
@@ -4,21 +4,25 @@ export default class User extends React.Component{
         console.warn("Constructor");
         super();
         this.state = {
-            data: "Santosh"
+            counter: 0
         }
     }
     componentDidMount(){
         console.warn("Component did Mount");
     }
     componentDidUpdate(preProps,preState,snapshot){
-        console.warn("Component Did Update",preState.data);
+        console.warn("Component Did Update",preState.counter,this.state.counter);
+        if (preState.counter === this.state.counter) {
+            alert("Same");
+        }
     }
     render(){
         console.warn("Render");
         return(
             <>
-                <h1>{this.state.data}</h1>
-                <button onClick={()=>this.setState({data:"Anvi"})}>Change Name</button>
+                <h1>{this.state.counter}</h1>
+                {/* <button onClick={()=>this.setState({counter:this.state.counter+1})}>Change Name</button> */}
+                <button onClick={()=>this.setState({counter:1})}>Change Name</button>
             </>
         );
     }
