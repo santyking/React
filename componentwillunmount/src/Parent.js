17a3d9f9cdc0bceaf6c36f48f88c6486efcb7c96
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2024-06-28 00:00:58 +0530

    Done

--- componentwillunmount/src/Parent.js
+++ componentwillunmount/src/Parent.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react'
 import Student from './Student'
 
 export default class Parent extends Component {
+    constructor(){
+        super();
+        this.state = {
+            show : true
+        }
+    }
   render() {
     return (
         <>
-            <Student></Student>
-            <button>Toggle</button>
+            {
+                this.state.show?
+                <Student></Student>:
+                null
+            }
+            <button onClick={()=>this.setState({show:!this.state.show})}>Toggle</button>
         </>
     )
   }
