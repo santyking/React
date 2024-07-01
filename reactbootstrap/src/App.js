573f13c2ea35a896aa5704a58bb1752c7de124a9
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-07-01 23:56:35 +0530

    initial commit

---
+++ reactbootstrap/src/App.js
@@ -0,0 +1,15 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <>
+      <h1>Hello!</h1>
+      <Button variant="dark">Dark</Button>
+    </>
+  );
+}
+
+export default App;
