211884d0692fc9306810acdf15b4c16efb771e28
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-07-02 22:52:26 +0530

    palindrome practice done

--- palindrome/src/App.js
+++ palindrome/src/App.js
@@ -3,27 +3,42 @@ import './App.css';
 import React,{useState} from 'react';
 
 function App() {
-  const [name,setName] = useState('');
-  const [palindrome,setPlaindrome] = useState(null);
-  
-  function checkPalindrome(e) {
-    setName(e.target.value);
-    // console.log(name);
-    checkPalindrome(e.target.value);
-  }
-  function checkPalindrome(str) {
-    const data = str.toLowerCase();
-    console.log(data);
-  }
+  const [input,setInput] = useState("");
+  const [palindrome,setPalindrome] = useState(false);
+  const [display,setDisplay] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Palindrome checker</h1>
-        <input type="text" placeholder='Enter Name' onChange={checkPalindrome}/>
+        <h1>Palindrome Checker</h1>
+        <input type="text" value={input} onChange={(e)=>setInput(e.target.value)}/> <br></br>  
+        <button onClick={()=>
+          {
+            setDisplay(true);
+            const data = input;
+            const finaldata = data.toLowerCase();
+            const revdata = data.toLowerCase().split('').reverse().join('');
+            console.log(finaldata);
+            console.log(revdata);
+            if(finaldata === revdata){
+              setPalindrome(true);
+            }
+            else{
+              setPalindrome(false);
+            }
+          }
+
+        }>Check</button>
         {
-          palindrome?
-          <h2>Palindrome</h2>:
-          <h2>Not a Palindrome</h2>
+          display?
+          <>
+            {
+              palindrome?
+              <h2>Palindrome</h2>
+              :
+              <h2>Not a Palindrome</h2>
+            }
+          </>
+          :null
         }
       </header>
     </div>
