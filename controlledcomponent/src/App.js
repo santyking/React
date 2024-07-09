47197cc4b2272db8f827b29023ee7959d8faf2b2
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2024-07-10 00:00:19 +0530

    controlled component

--- controlledcomponent/src/App.js
+++ controlledcomponent/src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react'
 
 function App() {
+  const [data,setData]  =useState("Santosh");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Controlled Component</h1>
+        <input type="text" value={data} onChange={(e)=>setData(e.target.value)}/>
       </header>
     </div>
   );
