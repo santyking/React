a1aa18a989b7dee0e8a03b8900e59626adb4020a
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2024-07-11 00:07:29 +0530

    uncontrolled component

--- uncontrolledcomponent/src/App.js
+++ uncontrolledcomponent/src/App.js
@@ -2,16 +2,27 @@ import React,{useRef} from 'react';
 import './App.css';
 
 function App() {
-  const myRef = useRef(null);
+  const myRef1 = useRef(null);
+  const myRef2 = useRef(null);
+  function submitForm(e) {
+    e.preventDefault();
+    console.log(myRef1.current.value);
+    console.log(myRef2.current.value);
+    let input3 = document.getElementById("my3").value;
+    console.log(input3);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Uncontrolled Component</h1>
-        <input type="text" ref={myRef}/>
-        <button>Change</button>
+        <form action="submitForm">
+          <h1>Uncontrolled Component</h1>
+          <input type="text" id='my1' ref={myRef1}/>
+          <input type="text" id='my2' ref={myRef2}/>
+          <input type="text" id='my3'/>
+          <button onClick={submitForm}>Change</button>
+        </form>
       </header>
     </div>
   );
 }
-
 export default App;
