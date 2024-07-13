767a2797f859b649d1e3749c685e47bb75f2135d
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-07-13 22:40:27 +0530

    high order component

--- higherordercomponent/src/App.js
+++ higherordercomponent/src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Higher Order Component</h1>
+        <HOCRed cmp={Counter}></HOCRed>
+        <HOCGreen cmp={Counter}></HOCGreen>
+        <HOCBlue cmp={Counter}></HOCBlue>
       </header>
     </div>
   );
+}
+
+function HOCRed(props){
+  return(
+    <h2 style={{backgroundColor:'red',width:100}}><props.cmp/></h2>
+  );
+}
+function HOCGreen(props){
+  return(
+    <h2 style={{backgroundColor:'green',width:100}}><props.cmp/></h2>
+  );
+}
+function HOCBlue(props){
+  return(
+    <h2 style={{backgroundColor:'blue',width:100}}><props.cmp/></h2>
+  );
+}
+
+function Counter() {
+  const [count,setCount] = useState(0);
+  return(
+    <div>
+      <h2>{count}</h2>
+      <button onClick={()=>setCount(count+1)}>Count</button>
+    </div>
+  ); 
 }
 
 export default App;
