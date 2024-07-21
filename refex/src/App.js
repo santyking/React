3538e0ab94e0f11d12b0b9d7918ec22afb51cd6c
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2024-07-21 23:59:22 +0530

    practice on react router

--- refex/src/App.js
+++ refex/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import React,{createRef} from 'react';
+import {Routes,Route,BrowserRouter} from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+
 class App extends React.Component {
   constructor(){
     super();
@@ -20,6 +24,12 @@ class App extends React.Component {
           <h1>Use Ref</h1>
           <input type="text" ref={this.inputref}/>
           <button onClick={()=>this.Clickme()}>Get</button>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Home></Home>}></Route>
+              <Route path='/about' element={<About></About>}></Route>
+            </Routes>
+          </BrowserRouter>
         </header>
       </div>
     );
