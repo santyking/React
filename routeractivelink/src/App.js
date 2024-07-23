c77c7e6f6667698ec7343c592a510e408bf05f4f
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2024-07-24 00:04:25 +0530

    active link practice done

--- routeractivelink/src/App.js
+++ routeractivelink/src/App.js
@@ -15,7 +15,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home></Home>}></Route>
             <Route path='/about' element={<About></About>}></Route>
-            <Route path='/user/:name' element={<About></About>}></Route>
+            <Route path='/user/:name' element={<User></User>}></Route>
           </Routes>
         </BrowserRouter>
       </header>
--- routeractivelink/src/Navbar.js
+++ routeractivelink/src/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <div>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/user/santosh'>Santosh</Link>
-        <Link to='/user/anvi'>Anvi</Link>
+        <NavLink to='/' className="nav-link">Home</NavLink>
+        <NavLink to='/about' className="nav-link">About</NavLink>
+        <NavLink to='/user/santosh' className="nav-link">Santosh</NavLink>
+        <NavLink to='/user/anvi' className="nav-link">Anvi</NavLink>
     </div>
   )
 }
