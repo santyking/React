aa82d988eb0a7ddf22adad876bd2c70950d108ac
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2024-07-27 23:59:14 +0530

    practice

--- programiticallylink/src/App.js
+++ programiticallylink/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import About from './About';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={}></Route>
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
