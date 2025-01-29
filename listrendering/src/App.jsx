06b35519aa3dfb6e6caae6415af4e9b2c18f6614
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2025-01-29 23:49:01 +0530

    practice done on list rendering

---
+++ listrendering/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import PersonComponent from './components/PersonComponent'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <PersonComponent />
+    </>
+  )
+}
+
+export default App
