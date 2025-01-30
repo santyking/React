c32fbaacb15081b05ff00ae5295e436d9892a961
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2025-01-31 00:00:01 +0530

    css practice

---
+++ cssreact/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MyComponent from './assets/components/MyComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <MyComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ cssreact/src/assets/components/MyComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from '.\'
+export default function MyComponent() {
+  return (
+    <div className='myPrimary'>MyComponent</div>
+  )
+}
