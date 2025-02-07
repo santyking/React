c7c7bc91cf7a9321fa58e1900af9d238acf474da
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2025-02-07 22:25:55 +0530

    react portals practice

---
+++ reactportals/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import PortalComponent from './components/PortalComponent'
+
+function App() {
+  return (
+    <>
+      <PortalComponent/>
+    </>
+  )
+}
+
+export default App
