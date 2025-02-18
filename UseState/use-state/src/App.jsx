01651515a88ffcdc40b3708a99a61ee7d16d88fe
Santosh Mhatre <santosh.mhatre93@gmail.com>
2025-02-19 00:08:01 +0530

    Object practice

--- UseState/use-state/src/App.jsx
+++ UseState/use-state/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import FirstComponent from './components/FirstComponent'
+import ArrayComponent from './components/ArrayComponent'
+import ObjComponent from './components/ObjComponent'
 import './App.css'
 
 function App() {
   return (
     <>
-      <FirstComponent/>
+      {/* <FirstComponent/> */}
+      {/* <ArrayComponent></ArrayComponent> */}
+      <ObjComponent/>
     </>
   )
 }
---
+++ UseState/use-state/src/components/ArrayComponent.jsx
@@ -0,0 +1,26 @@
+import React,{useState} from 'react'
+
+function ArrayComponent() {
+    const [students,setStudents] = useState([]);
+    const adder = () =>{
+        setStudents([...students,{
+            id: students.length,
+            value: Math.floor(Math.random() * 10) + 1
+        }])
+    }
+  return (
+    <div>
+        <h1>Array Component</h1>
+        <button onClick={adder}>Add</button>
+        <ul>
+            {
+                students.map(student=>
+                    (<li key={student.id}>{student.value}</li>)
+                )
+            }
+        </ul>
+    </div>
+  )
+}
+
+export default ArrayComponent
