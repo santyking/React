85eb79aa665ae4f432f1e8eb0492776c516cf5bb
Santosh Mhatre <santosh.mhatre93@gmail.com>
2025-02-27 00:46:45 +0530

    practice completed on cleanup  using useeffect

--- useeffectreact/src/App.jsx
+++ useeffectreact/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import Counter from './components/Counter'
 import './App.css'
+import HookMouse from './components/HookMouse'
+import HookMouse2 from './components/HookMouse2'
+import MouseContainer from './components/MouseContainer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Counter/>
+      {/* <Counter/> */}
+      {/* <HookMouse2/> */}
+      <MouseContainer/>
     </>
   )
 }
