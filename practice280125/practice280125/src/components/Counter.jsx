a5af7d8babbc7ce17d6a16adebf0f542adf800ff
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2025-02-28 22:41:03 +0530

    practice done

--- practice280125/practice280125/src/components/Counter.jsx
+++ practice280125/practice280125/src/components/Counter.jsx
@@ -1,17 +1,25 @@
-import React,{ useState } from 'react'
+import React, { useState, useEffect } from "react";
 
-export default function Counter() {
-    const [count,setCount]  = useState(0);
-    function increment(){
-        setCount(prevCount => prevCount+1,()=>{
-            console.log(count);
-        });
-        console.log(count);
-    }
+function Counter() {
+  const [count, setCount] = useState(0);
+  const tick = () => {
+    setCount((prevState) => prevState + 1);
+  };
+  useEffect(() => {
+    const intervalId = setInterval(tick, 1000);
+    return () => clearInterval(intervalId);
+  }, [count]);
+  function PauseTimer() {
+    clearInterval(intervalId);
+  }
   return (
     <>
-    <div>Count is {count}</div>
-    <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
+      <button>Start</button>
+      <button onClick={PauseTimer}>Pause</button>
+      <button>Reset</button>
     </>
-  )
+  );
 }
+
+export default Counter;
