ca41227561c1273e41cd9c6a9a534e669ae9be05
Santosh Mhatre <santosh.mhatre93@gmail.com>
2025-03-06 23:19:19 +0530

    context reducer practice

--- contextreducer/src/components/ComponentD.jsx
+++ contextreducer/src/components/ComponentD.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { CountContext } from '../App'
 
 function ComponentD() {
+  const myCountContext = useContext(CountContext);
   return (
-    <div>ComponentD</div>
+    <div>ComponentD
+      <button onClick={()=>myCountContext.countDispatch('increment')}>+</button>
+        <button onClick={()=>myCountContext.countDispatch('decrement')}>-</button>
+        <button onClick={()=>myCountContext.countDispatch('reset')}>0</button>
+    </div>
   )
 }
 
--- contextreducer/src/components/ComponentF.jsx
+++ contextreducer/src/components/ComponentF.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { CountContext } from '../App'
 
 function ComponentF() {
+  const myCountContext = useContext(CountContext);
   return (
-    <div>ComponentF</div>
+    <div>ComponentF
+      <button onClick={()=>myCountContext.countDispatch('increment')}>+</button>
+        <button onClick={()=>myCountContext.countDispatch('decrement')}>-</button>
+        <button onClick={()=>myCountContext.countDispatch('reset')}>0</button>
+    </div>
   )
 }
 
