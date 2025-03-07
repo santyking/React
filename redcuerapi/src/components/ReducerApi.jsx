d785c38718030fdf7fb35ae0f8d0a65bb82e7f01
Santosh Mhatre <santosh.mhatre93@gmail.com>
2025-03-08 00:29:22 +0530

    use reduer api practice

--- redcuerapi/src/components/ReducerApi.jsx
+++ redcuerapi/src/components/ReducerApi.jsx
@@ -1,15 +1,50 @@
+import { type } from '@testing-library/user-event/dist/type'
 import axios from 'axios'
-import React,{useState,useEffect,useReducer} from 'react'
+import React,{useEffect,useReducer} from 'react'
 
+const initialState = {
+    loading:true,
+    error:'',
+    post:{}
+}
+const reducer = (state,action)=>{
+    switch(action.type){
+        case 'FETCH_SUCCESS':
+            return {
+                loading:false,
+                error:'',
+                post:action.payload
+            }
+        case 'FETCH_ERROR':
+            return {
+                loading:false,
+                error: 'Something went wrong',
+                post : {}
+            }
+        default:
+            return state
+    }
+}
 function ReducerApi() {
-useEffect(()=>{
-    axios.get(`https://jsonplaceholder.typicode.com/posts`)
-    .then()
-})
+    const [state,action] = useReducer(reducer,initialState);
+    useEffect(()=>{
+        axios.get(`https://jsonplaceholder.typicode.com/posts/1`)
+        .then(response=>{
+            action({type:'FETCH_SUCCESS',payload:response.data})
+        })
+        .catch(error=>{
+            action({type:'FETCH_ERROR'})
+        })
+    },[])
   return (
     <div>
         <h1>API Call with Reducer</h1>
-
+        {
+            state.loading ? 'Loading' : state.post.title
+        }
+        {
+            state.error ? error : null
+        }
     </div>
   )
 }
