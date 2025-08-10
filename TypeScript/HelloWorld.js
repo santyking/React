631fdf944063abb5377a8534e2982a974f8c2281
Santosh Mhatre <santosh.mhatre93@gmail.com>
2025-08-10 23:47:20 +0530

    practice on ts

--- TypeScript/HelloWorld.js
+++ TypeScript/HelloWorld.js
@@ -1,11 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var message = "Hello there";
-console.log(message);
-var x = 10;
-x = 101;
-console.log(x);
-var status = true;
-var username = "Santosh";
-var sentence = "Hello ".concat(username, "\nHOW ARE You");
-console.log(sentence);
+// let message:string="Hello there";
+// console.log(message);
+// let x:number = 10;
+// x = 101;
+// console.log(x);
+// let status:boolean = true;
+// const username:string = "Santosh";
+// const sentence:string = `Hello ${username}
+// HOW ARE You`
+// console.log(sentence);
+var userids = [1, 2, 3];
+userids.map(function (number) { return console.log(number); });
--- TypeScript/HelloWorld.ts
+++ TypeScript/HelloWorld.ts
@@ -1,11 +1,13 @@
 export {};
-let message:string="Hello there";
-console.log(message);
-let x:number = 10;
-x = 101;
-console.log(x);
-let status:boolean = true;
-const username:string = "Santosh";
-const sentence:string = `Hello ${username}
-HOW ARE You`
-console.log(sentence);
+// let message:string="Hello there";
+// console.log(message);
+// let x:number = 10;
+// x = 101;
+// console.log(x);
+// let status:boolean = true;
+// const username:string = "Santosh";
+// const sentence:string = `Hello ${username}
+// HOW ARE You`
+// console.log(sentence);
+let userids:number[] = [1,2,3];
+userids.map(number=>console.log(number));
