4558e422e5e0a880f44ded6884cfefeaaf9c843f
Santosh Mhatre <santosh.mhatre93@gmail.com>
2025-08-11 23:44:14 +0530

    practice

---
+++ TypeScript/Arrays.js
@@ -0,0 +1,7 @@
+// let names:readonly string[] = ["Santosh","Ankita"];
+var names = ["Santosh", "Ankita"];
+// names.push("Anvi");
+// let fname:string|undefined =  names.pop();
+names.push("Anvi");
+names.map(function (name) { return console.log(name); });
+// names.push(2);
---
+++ TypeScript/Arrays.ts
@@ -0,0 +1,7 @@
+// let names:readonly string[] = ["Santosh","Ankita"];
+const names:string[] = ["Santosh","Ankita"];
+// names.push("Anvi");
+// let fname:string|undefined =  names.pop();
+names.push("Anvi");
+names.map(name=>console.log(name));
+// names.push(2);
